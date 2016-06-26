c8b10d74a90c51fea6f9c6d32afc2ff50d71ae86
Liang Zhang <psychelzh@gmail.com>
2016-06-26 21:46:26 +0800

    Bug fixes and new idea in conflict-related tasks.
    1. Further bug fixes for better backward-compatibility for metadata
    analysis.
    2. Adding v-CongEffect (drifting rate, might be abandoned in later
    analysis) when analyzing conflict tasks.
    3. Minor bug fixes.

--- utilis/sngprocConflict.m
+++ utilis/sngprocConflict.m
@@ -1,8 +1,8 @@
-function res = sngprocConflict(RECORD, varPref, delimiter, varSuff)
+function res = sngprocConflict(RECORD)
 %SNGPROCCONFLICT does some basic data transformation to conflict-based tasks.
 %
 %   Basically, the supported tasks are as follows:
-%     Flanker, Stroop1-2, NumStroop TaskSwicthing.
+%     Flanker, Stroop1-2, NumStroop, TaskSwicthing.
 %   The output table contains 8 variables.
 
 %By Zhang, Liang. 04/13/2016. E-mail:psychelzh@gmail.com
@@ -11,19 +11,19 @@ res = table;
 congcode = 1;
 incongcode = 2;
 %Overall RT and ACC.
-res.RT_Overall = mean(RECORD.RT(RECORD.ACC == 1));
+res.MRT_Overall = mean(RECORD.RT(RECORD.ACC == 1));
 res.ACC_Overall = length(RECORD.ACC(RECORD.ACC == 1)) / length(RECORD.ACC);
 %Condition-wise analysis. Here the abnormal trials are included.
 %Condition of congruent/repeat.
-res.([varPref{1}, delimiter, varSuff{2}]) = mean(RECORD.RT(RECORD.SCat == congcode & RECORD.ACC == 1));
-res.([varPref{2}, delimiter, varSuff{2}]) = ...
-    length(RECORD.ACC(RECORD.ACC == 1 & RECORD.SCat == congcode)) / length(RECORD.ACC(RECORD.SCat == congcode));
+congtrials    = RECORD(RECORD.SCat == congcode, :);
+res_congruent = sngprocEZDiff(congtrials);
+res_congruent.Properties.VariableNames = strcat(res_congruent.Properties.VariableNames, '_Congruent');
 %Condition of incongruent/switch.
-res.([varPref{1}, delimiter, varSuff{3}]) = nanmean(RECORD.RT(RECORD.SCat == incongcode & RECORD.ACC == 1));
-res.([varPref{2}, delimiter, varSuff{3}]) = ...
-    length(RECORD.ACC(RECORD.ACC == 1 & RECORD.SCat == incongcode)) / length(RECORD.ACC(RECORD.SCat == incongcode));
+incongtrials    = RECORD(RECORD.SCat == incongcode, :);
+res_incongruent = sngprocEZDiff(incongtrials);
+res_incongruent.Properties.VariableNames = strcat(res_incongruent.Properties.VariableNames, '_Incongruent');
 %The last two output variables for conflict effect.
-res.([varPref{1}, delimiter, varSuff{4}]) = ...
-    res.([varPref{1}, delimiter, varSuff{3}]) - res.([varPref{1}, delimiter, varSuff{2}]);
-res.([varPref{2}, delimiter, varSuff{4}]) = ...
-    res.([varPref{2}, delimiter, varSuff{2}]) - res.([varPref{2}, delimiter, varSuff{3}]);
+res = [res, res_congruent, res_incongruent];
+res.MRT_CongEffect = res.MRT_Incongruent - res.MRT_Congruent;
+res.ACC_CongEffect = res.ACC_Congruent - res.ACC_Incongruent;
+res.v_CongEffect   = res.v_Congruent - res.v_Incongruent;
