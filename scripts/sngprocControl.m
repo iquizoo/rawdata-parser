ca9f0d8e5ff5a57b5190e86b780b21326011c7ed
Zhang Liang <psychelzh@gmail.com>
2017-10-09 17:55:16 +0800

    FIX BUG: fixed bugs in 'Control'. 1. ACC cannot be set as NaN before processing. 2. Condition labels will be appended at the index names now.

--- scripts/sngprocControl.m
+++ scripts/sngprocControl.m
@@ -11,8 +11,6 @@ function [stats, labels] = sngprocControl(SCat, RT, ACC)
 
 RT(ACC == -1) = NaN;
 RT = rmoutlier(RT);
-% set ACC of outlier and -1 trials as NaN (not included)
-ACC(isnan(RT) | ACC == -1) = NaN;
 
 [total_stats, total_labels] = SAT(RT, ACC);
 lisas_weight = total_stats(ismember(total_labels, 'lisas_weight'));
@@ -22,13 +20,13 @@ lisas_weight = total_stats(ismember(total_labels, 'lisas_weight'));
     RT, ACC, grps);
 diff_stats = cond_stats(2, :) - cond_stats(1, :);
 diff_labels = strcat('diff_', cond_labels(1, :));
-cond_labels = strcat(repmat(cellstr(gid), 1, size(cond_labels, 2)), '_', cond_labels);
+cond_labels = strcat(cond_labels, '_', repmat(cellstr(gid), 1, size(cond_labels, 2)));
 
 PC = 1 - total_stats(ismember(total_labels, 'PE'));
-RT = cond_stats(ismember(cond_labels, 'Incon_MRT'));
+RT = cond_stats(ismember(cond_labels, 'MRT_Incon'));
 RT = max(min(RT, 2500), 500);
 NIHScore = asin(sqrt(PC)) / (pi / 2) + ...
     (log(2500) - log(RT)) / (log(2500) - log(500));
 
-stats = [total_stats, reshape(cond_stats, 1, numel(cond_stats)), diff_stats, NIHScore];
-labels = [total_labels, reshape(cond_labels, 1, numel(cond_labels)), diff_labels, {'NIHScore'}];
+stats = [total_stats, cond_stats(:)', diff_stats, NIHScore];
+labels = [total_labels, cond_labels(:)', diff_labels, {'NIHScore'}];
