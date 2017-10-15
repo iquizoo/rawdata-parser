94b545978a98fcb872245c29fa44b4e749cd2aac
Zhang Liang <psychelzh@gmail.com>
2017-10-15 22:28:51 +0800

    FIX BUG: Fix two issues for 'Control': conditional outlier removal and relative reference.

--- scripts/sngprocControl.m
+++ scripts/sngprocControl.m
@@ -9,24 +9,30 @@ function [stats, labels] = sngprocControl(SCat, RT, ACC)
 
 %By Zhang, Liang. 04/13/2016. E-mail:psychelzh@gmail.com
 
+% find group information
+[grps, gid] = findgroups(SCat);
+% remove RT's with no response
 RT(ACC == -1) = NaN;
-RT = rmoutlier(RT);
-
+% remove RT outlier for each condition
+for igrp = 1:length(gid)
+    curGroupIdx = grps == igrp;
+    RT(curGroupIdx) = rmoutlier(RT(curGroupIdx));
+end
+% calculate statistics for the whole task
 [total_stats, total_labels] = SAT(RT, ACC);
 lisas_weight = total_stats(ismember(total_labels, 'lisas_weight'));
-
-[grps, gid] = findgroups(SCat);
+% calculate statistics for each condition
 [cond_stats, cond_labels] = splitapply(@(x, y) SAT(x, y, lisas_weight), ...
     RT, ACC, grps);
 diff_stats = cond_stats(2, :) - cond_stats(1, :);
 diff_labels = strcat(cond_labels(1, :), '_diff');
-cond_labels = strcat(cond_labels, '_', repmat(cellstr(gid), 1, size(cond_labels, 2)));
-
-PC = 1 - total_stats(ismember(total_labels, 'PE'));
-RT = cond_stats(ismember(cond_labels, 'MRT_Incon'));
-RT = max(min(RT, 2500), 500);
-NIHScore = asin(sqrt(PC)) / (pi / 2) + ...
-    (log(2500) - log(RT)) / (log(2500) - log(500));
-
+cond_labels_fix = strcat(cond_labels, '_', repmat(cellstr(gid), 1, size(cond_labels, 2)));
+% calculate NIHScore
+PC_Total = 1 - total_stats(ismember(total_labels, 'PE'));
+MRT2 = cond_stats(2, ismember(cond_labels(1, :), 'MRT'));
+MRT2 = max(min(MRT2, 2500), 500);
+NIHScore = asin(sqrt(PC_Total)) / (pi / 2) + ...
+    (log(2500) - log(MRT2)) / (log(2500) - log(500));
+% merge results
 stats = [total_stats, cond_stats(:)', diff_stats, NIHScore];
-labels = [total_labels, cond_labels(:)', diff_labels, {'NIHScore'}];
+labels = [total_labels, cond_labels_fix(:)', diff_labels, {'NIHScore'}];
