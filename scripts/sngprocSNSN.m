affe32225ef3aaa60493a8e8202214b041d6250b
Zhang Liang <psychelzh@gmail.com>
2017-12-07 14:17:35 +0800

    FIX BUG: fix an issue when SCat is not complete

--- scripts/sngprocSNSN.m
+++ scripts/sngprocSNSN.m
@@ -12,6 +12,18 @@ RT = rmoutlier(RT);
 % set ACC of outlier and -1 trials as NaN (not included)
 ACC(isnan(RT) | ACC == -1) = NaN;
 NInclude = sum(~isnan(ACC));
+% fix incomplete SCat
+cats = categories(SCat);
+switch length(unique(SCat))
+    case 0
+        % no SCat data found, compose one
+        ACC = nan(2, 1);
+        SCat = categorical(cats);
+    case 1
+        % only one SCat data found, add th missing one
+        ACC = [ACC; nan];
+        SCat = [SCat; setdiff(cats, unique(SCat))];
+end
 % get the number of hit and false alarm
 [grps, gid] = findgroups(SCat);
 [cond_include, cond_correct] = grpstats(ACC, grps, {'numel', @(x) sum(x == 1)});
