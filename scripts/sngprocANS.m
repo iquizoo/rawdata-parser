cba241204f68661267afc5bdda1063fc78905172
Zhang Liang <psychelzh@gmail.com>
2018-04-26 18:34:57 +0800

    Add percent of correct to result of numerosity task

--- scripts/sngprocANS.m
+++ scripts/sngprocANS.m
@@ -9,20 +9,22 @@ NResp = sum(ACC ~= -1);
 % accuracy is of interest, so trials with no response or too quick response
 % will be treated as incorrect ones
 ACC(ACC == -1 | RT < 100) = 0;
+% get the percent of correct
+PC = 1 - mean(ACC);
 % get the stimuli matrix
 S = [S1, S2];
 larger = max(S, [], 2);
 smaller = min(S, [], 2);
 % get the accuracy measure for each type of stimuli
 [grps, gidl, gids] = findgroups(larger, smaller);
-Pc = grpstats(ACC, grps);
+Pc_grp = grpstats(ACC, grps);
 % model fitting
 warning('off', 'stats:nlinfit:ModelConstantWRTParam')
 warning('off', 'MATLAB:rankDeficientMatrix')
 mdlfun = @(b, x) 1 - 1 / 2 .* ...
     erfc((x(:, 1) - x(:, 2)) ./ (b .* sqrt(2 .* (x(:, 1) .^ 2 + x(:, 2) .^ 2))));
-mdl = fitnlm([gidl, gids], Pc, mdlfun, 1);
+mdl = fitnlm([gidl, gids], Pc_grp, mdlfun, 1);
 w = mdl.Coefficients.Estimate;
 % compose return values
-stats = [NTrial, NResp, w];
-labels = {'NTrial', 'NResp', 'w'};
+stats = [NTrial, NResp, PC, w];
+labels = {'NTrial', 'NResp', 'PC', 'w'};
