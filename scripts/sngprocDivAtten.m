820ac51100fc9da26a12c4b976de90cdb6e6b87c
Zhang Liang <psychelzh@gmail.com>
2018-06-22 20:02:04 +0800

    Add analysis support for DivAtten task

---
+++ scripts/sngprocDivAtten.m
@@ -0,0 +1,14 @@
+function [stats, labels] = sngprocDivAtten(Condition, RT, ACC, STIM)
+%SNGPRODIVATTEN analyzes data of divided attention task
+
+% for now, the CResp is not recorded, will use arbitrary value as target.
+% treat one of STIM as target (the first of unique results)
+SCat = categorical(STIM, unique(STIM), {'Target', 'Non-Target'});
+% calculate the same stats as CPT1 for each condition
+[grps, gid] = findgroups(Condition);
+[cond_stats, cond_labels] = splitapply(@sngprocCPT1, RT, ACC, SCat, grps);
+keyIdxName = 'dprime';
+keyIdx = mean(cond_stats(ismember(cond_labels, keyIdxName)));
+cond_labels_fix = strcat(cond_labels, '_', repmat(cellstr(gid), 1, size(cond_labels, 2)));
+stats = [cond_stats(:)', keyIdx];
+labels = [cond_labels_fix(:)', 'dprimeUnion'];
