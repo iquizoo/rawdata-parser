0fbf59b830a8434e6d83d5384b5dfb9bfc084637
Zhang Liang <psychelzh@gmail.com>
2018-07-01 16:54:07 +0800

    Fix an exception occured when not enough data for each condition
    These tasks featured two conditions, say 'congruent' and 'incongruent' conditions. But sometimes data of one condition might have been lost, or the configuration just errored.

--- scripts/sngprocControl.m
+++ scripts/sngprocControl.m
@@ -9,6 +9,13 @@ function [stats, labels] = sngprocControl(RT, ACC, SCat)
 
 % By Zhang, Liang. 04/13/2016. E-mail:psychelzh@gmail.com
 
+% fix data to ensure no missing data from each condition
+cond_name = categories(SCat);
+cond_real = unique(SCat);
+cond_missing = setdiff(cond_name, cond_real);
+RT = vertcat(RT, nan(numel(cond_missing), 1));
+ACC = vertcat(ACC, nan(numel(cond_missing), 1));
+SCat = vertcat(SCat, cond_missing);
 % find group information
 [grps, gid] = findgroups(SCat);
 % remove RT's with no response
@@ -27,7 +34,7 @@ lisas_weight = total_stats(ismember(total_labels, 'lisas_weight'));
     RT, ACC, grps);
 diff_stats = cond_stats(2, :) - cond_stats(1, :);
 diff_labels = strcat(cond_labels(1, :), '_diff');
-cond_labels_fix = strcat(cond_labels, '_', repmat(cellstr(gid), 1, size(cond_labels, 2)));
+cond_labels_fix = strcat(cond_labels, '_', repmat(cond_name, 1, size(cond_labels, 2)));
 % calculate NIHScore
 PC_Total = 1 - total_stats(ismember(total_labels, 'PE'));
 MRT2 = cond_stats(2, ismember(cond_labels(1, :), 'MRT'));
